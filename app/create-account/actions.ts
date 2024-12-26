2433e6461a337ae43cf686d1bc1e1b397bfb6922
신광수 <kwangsu.shin@saltlux.com>
2024-12-26 16:55:33 +0900

    setup prisma

---
+++ app/create-account/actions.ts
@@ -0,0 +1,40 @@
+"use server"
+
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import { z } from "zod";
+
+const checkUsername = (username: string) => !username.includes("potato")
+
+const checkPassword = ({password, confirmPassword}: {password: string, confirmPassword: string}) => password === confirmPassword
+
+const formSchema = z.object({
+  username: z.string({
+    invalid_type_error: "Username must be a string",
+    required_error: "Where is my username?"
+  })
+  .toLowerCase()
+  .trim()
+  .refine(checkUsername, "potato is not allowed"),
+  email: z.string().email().toLowerCase(),
+  password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+  confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
+}).refine(checkPassword, {
+  message: "password should be same",
+  path: ["confirmPassword"]
+})
+
+export async function createAccount(prevState: unknown, formData: FormData) {
+    const data = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    }
+
+    const result = formSchema.safeParse(data)
+    if (!result.success) {
+      return result.error.flatten()
+    } else {
+      console.log(result.data)
+    }
+}

31f9d9f2cf5f74b4d715bf561766fc2aa867f83e
신광수 <kwangsu.shin@saltlux.com>
2024-12-27 17:10:31 +0900

    그룹 라우팅 적용 & PRODUCT 작업 시작

--- app/profile/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Profile() {
-  return (
-    <h1>Welcome</h1>
-  )
-}
---
+++ components/tab-bar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+  HomeIcon as OutlineHomeIcon,
+  NewspaperIcon as OutlineNewspaperIcon,
+} from "@heroicons/react/24/outline";
+import {
+  HomeIcon as SolidHomeIcon,
+  NewspaperIcon as SolidNewspaperIcon,
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function TabBar() {
+  const pathName = usePathname();
+
+  return (
+    <div className="fixed bottom-0 w-full mx-auto max-w-screen-md grid grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white">
+      <Link href="/products" className="flex flex-col items-center gap-px">
+        {pathName === "/products" ? (
+          <SolidHomeIcon className="w-7 h-7" />
+        ) : (
+          <OutlineHomeIcon className="w-7 h-7" />
+        )}
+        <span>홈</span>
+      </Link>
+      <Link href="/life" className="flex flex-col items-center gap-px">
+        {pathName === "/life" ? (
+          <SolidNewspaperIcon className="w-7 h-7" />
+        ) : (
+          <OutlineNewspaperIcon className="w-7 h-7" />
+        )}
+        <span>동네생활</span>
+      </Link>
+      <Link href="/chat" className="flex flex-col items-center gap-px">
+        {pathName === "/chat" ? (
+          <SolidHomeIcon className="w-7 h-7" />
+        ) : (
+          <OutlineHomeIcon className="w-7 h-7" />
+        )}
+        <span>채팅</span>
+      </Link>
+      <Link href="/live" className="flex flex-col items-center gap-px">
+        {pathName === "/live" ? (
+          <SolidHomeIcon className="w-7 h-7" />
+        ) : (
+          <OutlineHomeIcon className="w-7 h-7" />
+        )}
+        <span>쇼핑</span>
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center gap-px">
+        {pathName === "/profile" ? (
+          <SolidHomeIcon className="w-7 h-7" />
+        ) : (
+          <OutlineHomeIcon className="w-7 h-7" />
+        )}
+        <span>나의 당근</span>
+      </Link>
+    </div>
+  );
+}
